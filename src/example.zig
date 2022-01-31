54d6d53aab63aec3132803b807bcb8047c789599
Sebastian Keller <sebastiankeller@fastmail.net>
2022-01-31 22:42:10 +0100

    Changed log level from warn to debug for example

--- src/example.zig
+++ src/example.zig
@@ -54,32 +54,32 @@ pub fn main() void {
     ;
 
     var geojson = Parser.parse(json, std.testing.allocator) catch |err| {
-        std.log.warn("Could not parse geojson! {}", .{err});
+        std.log.err("Could not parse geojson! {}", .{err});
         return;
     };
     defer geojson.deinit();
 
     // switch on the content if you don't already know the type
     switch (geojson.content) {
-        .feature => std.log.warn("It's a Feature!\n", .{}),
-        .feature_collection => std.log.warn("It's a FeatureCollection!\n", .{}),
-        .geometry => std.log.warn("It's a Geometry!\n", .{}),
+        .feature => std.log.debug("It's a Feature!\n", .{}),
+        .feature_collection => std.log.debug("It's a FeatureCollection!\n", .{}),
+        .geometry => std.log.debug("It's a Geometry!\n", .{}),
     }
 
     // there are helper methods `featureCollection()`, `feature()`, and `geometry()`, returning optionals
     if (geojson.featureCollection()) |collection| {
-        std.log.warn("FeatureCollection contains {} features\n", .{collection.len});
+        std.log.debug("FeatureCollection contains {} features\n", .{collection.len});
 
         for (collection) |feature, idx| {
-            std.log.warn("{}: It's a {s} => ", .{ idx, @tagName(feature.geometry) });
+            std.log.debug("{}: It's a {s} => ", .{ idx, @tagName(feature.geometry) });
             switch (feature.geometry) {
-                .point => |value| std.log.warn("[{d}, {d}]\n", .{ value[0], value[1] }),
-                .multi_point => |value| std.log.warn("containing {} points\n", .{value.len}),
-                .line_string => |value| std.log.warn("containing {} points\n", .{value.len}),
-                .multi_line_string => |value| std.log.warn("containing {} lineStrings\n", .{value.len}),
-                .polygon => |value| std.log.warn("containing {} rings\n", .{value.len}),
-                .multi_polygon => |value| std.log.warn("containing {} polygons\n", .{value.len}),
-                .geometry_collection => |value| std.log.warn("containing {} geometries\n", .{value.len}),
+                .point => |value| std.log.debug("[{d}, {d}]\n", .{ value[0], value[1] }),
+                .multi_point => |value| std.log.debug("containing {} points\n", .{value.len}),
+                .line_string => |value| std.log.debug("containing {} points\n", .{value.len}),
+                .multi_line_string => |value| std.log.debug("containing {} lineStrings\n", .{value.len}),
+                .polygon => |value| std.log.debug("containing {} rings\n", .{value.len}),
+                .multi_polygon => |value| std.log.debug("containing {} polygons\n", .{value.len}),
+                .geometry_collection => |value| std.log.debug("containing {} geometries\n", .{value.len}),
                 .@"null" => continue,
             }
         }
@@ -88,12 +88,12 @@ pub fn main() void {
         const feature = collection[0];
         if (feature.properties) |properties| {
             if (properties.get("prop0")) |value| {
-                std.log.warn("Property: 'prop0' => {}\n", .{value});
+                std.log.debug("Property: 'prop0' => {}\n", .{value});
             }
         }
 
         // or unsafe
         const value = collection[1].properties.?.get("prop1").?;
-        std.log.warn("Property: 'prop1' => {}\n", .{value});
+        std.log.debug("Property: 'prop1' => {}\n", .{value});
     }
 }
